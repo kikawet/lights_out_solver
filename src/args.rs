5651ad6b77610a6cef359e3be8e334b3131d2bde
kikawet <lopezortegafrancisco1@gmail.com>
2022-05-28 13:33:58 +0200

    fixed arg issues

--- src/args.rs
+++ src/args.rs
@@ -1,19 +1,20 @@
 extern crate clap;
-use clap::{arg, builder::PossibleValuesParser, value_parser, Command};
+use clap::{Arg, builder::PossibleValuesParser, value_parser, command, Command};
 
 pub fn init_app() -> Command<'static> {
-    Command::new("Puzzle Solver")
+    command!()
+    .name("Lights Out Puzzle Solver")
     .version("0.1.0")
     .about("With the given input of on node it will output the order to toggle the lights to solve the puzzle") 
     .arg(
-        arg!("NODES")
+        Arg::new("NODES")
             .help("Indexes of the active nodes starting at 0 on the top left")
             .multiple_values(true)
             .index(1)
             .value_parser(value_parser!(usize)),
     )
     .arg(
-        arg!("rows")
+        Arg::new("rows")
             .help("The number of rows")
             .short('r')
             .value_name("rows")
@@ -22,7 +23,7 @@ pub fn init_app() -> Command<'static> {
             .value_parser(value_parser!(usize)),
     )
     .arg(
-        arg!("cols")
+        Arg::new("cols")
             .help("The number of columns")
             .short('c')
             .value_name("cols")
@@ -31,14 +32,14 @@ pub fn init_app() -> Command<'static> {
             .value_parser(value_parser!(usize)),
     )
     .arg(
-        arg!("verbose")
+        Arg::new("verbose")
             .help("Use verbose output")
             .short('v')
             .value_name("verbose")
             .takes_value(false)
     )
     .arg(
-        arg!("run_simulation")
+        Arg::new("run_simulation")
             .help("Run a simulation of the puzzle")
             .short('s')
             .value_name("postions_to_trigger")
@@ -47,7 +48,7 @@ pub fn init_app() -> Command<'static> {
             .value_parser(value_parser!(usize)),
     )
     .arg(
-        arg!("output_mode")
+        Arg::new("output_mode")
             .help("Sets the output mode")
             .short('m')
             .value_name("output_mode")
